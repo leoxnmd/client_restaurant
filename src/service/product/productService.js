acb8fa18743a25693a2af6a08638e9756fd8a391
leoxnmd <nmd.leo2001@gmail.com>
2023-05-22 08:21:33 +0700

    update cart

--- src/service/product/productService.js
+++ src/service/product/productService.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 import { base_url } from "../base/baseUrl";
+import { config } from "../base/axiosconfig";
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvd25lckBnbWFpbC5jb20iLCJpZCI6NjYsIm5hbWUiOiJDaOG7pyBj4butYSBow6BuZyIsInJvbGVzIjpbIk9XTkVSIl0sImlhdCI6MTY4NDQwMzU2MywiZXhwIjoxNjg0NDg5OTYzfQ.AIqMyhBAAMRDyfiIZ6k_tWLngqSlDSz3LHFAeMhBnw4';
-const getProducts = async () => {
-    const response = await axios.get(`${base_url}product/?page=1&size=15
-    `, token);
+export const getProducts = async () => {
+
+    const response = await axios.get(`${base_url}product/?page=1&size=15`, config);
     return response.data.data.content;
 };
-const productService = {
-    getProducts
-};
 
-export default productService;
